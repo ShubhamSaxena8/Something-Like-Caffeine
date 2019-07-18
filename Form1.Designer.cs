7c564cde87165c4a220714c0b3fa31f0cf329362
Shubham Saxena <Shub.s7802@gmail.com>
2019-07-18 15:08:22 +0530

    Uploading all files

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+
+namespace TestWPApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(251, 183);
+            this.button1.MaximumSize = new System.Drawing.Size(40, 23);
+            this.button1.MinimumSize = new System.Drawing.Size(25, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.Location = new System.Drawing.Point(15, 32);
+            this.textBox1.MaximumSize = new System.Drawing.Size(307, 168);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(276, 136);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.AutoSize = true;
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(15, 183);
+            this.button3.MaximumSize = new System.Drawing.Size(45, 23);
+            this.button3.MinimumSize = new System.Drawing.Size(27, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 229);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(319, 268);
+            this.MinimumSize = new System.Drawing.Size(168, 170);
+            this.Name = "Form1";
+            this.Text = "TestWFA";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWPApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int pos1 = 0;
+        int pos2 = 0;
+        System.Threading.Thread t;
+        Random random = new Random();
+        [STAThread]
+        private void DoThisAllTheTime()
+        {
+            while (true)
+            {
+               // you need to use Invoke because the new thread can't access the UI elements directly
+                textBox1.AppendText($"{DateTime.Now.ToString()}\r\n");
+                //Cursor.Position = new Point(random.Next(0,1366), random.Next(0, 738));
+                SendKeys.SendWait("{BREAK}");
+                System.Threading.Thread.Sleep(4000);
+                MethodInvoker mi = delegate () { this.Text = DateTime.Now.ToString(); };
+                this.Invoke(mi);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+                t = new System.Threading.Thread(DoThisAllTheTime); 
+                t.Start();
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            textBox1.AppendText($"\r\nKeyPress keychar: {e.KeyChar}" + "\r\n");
+            t.Abort();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            t.Abort();
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            textBox1.AppendText($"\r\nKeyPress keychar: {e.KeyChar}" + "\r\n");
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    SendKeys.SendWait("{J}");
+        //}
+    }
+}
